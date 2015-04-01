e00e9d9c98255814300e65f6c5b5a78af1c4f8a5
maxime peabody <maximepeabody@gmail.com>
2015-04-01 01:51:15 +0100

    changed controller code to page service

--- app/app.js
+++ app/app.js
@@ -28,161 +28,160 @@ angular.module('app',['facebook','app.filters', 'ngRoute'])
     })
     
 }])
-.controller('feedCtrl', ['$scope', 'Facebook', function($scope, Facebook){
-
-     $scope.getFeed = function() {
-        Facebook.api($scope.pageId + '?fields=feed.limit(10)', {access_token:$scope.token}, function(response) {
-            $scope.feed = response.feed;
-            for(var i = 0; i< $scope.feed.data.length; i++) {
-                $scope.setContent($scope.feed.data[i].object_id, $scope.feed.data[i].status_type, i);
+.factory('page', ['Facebook', function(Facebook) {
+    //page object containing all the info for the fb page//
+    var page = {};
+    page.pageId = '1425194534381693';
+    //to check on the facebook api being ready //
+    page.facebookReady = false;  
+    //what tabs are available, i.e home, about, contact
+    page.tabs = {};
+    page.tabs.gallery = false;
+    page.tabs.events = false;
+    page.token = '1427747097519995|85GJFSbBD6WbpVoF8EZavlLWKUE';
+    //var that = page;
+    
+    // get all the main content needed in one call //
+    page.loadMainContent = function() {
+        Facebook.api(page.pageId + '?fields=cover,name,feed,phone,about,emails,current_location,description,company_overview,hours,price_range,photos{picture,album}',{access_token: page.token}, function(response){
+            console.log(page);
+            page.name = response.name;
+            page.phone = response.phone;
+            page.about = response.about;
+            page.emails = response.emails;
+            page.currentLocation = response.current_location;
+            page.locations = response.locations;
+            page.description = response.description;
+            page.companyOverview = response.company_overview;
+            page.hours = response.hours;
+            page.priceRange = response.price_range;
+            page.getProfilePicture(response.photos.data[0].id);
+            page.setCover(response.cover.cover_id);
+            //page.feed = response.feed;
+            console.log(response);
+            });
+        };
+    page.getFeed = function() {
+        Facebook.api(page.pageId + '?fields=feed.limit(10)', {access_token:page.token}, function(response) {
+            page.feed = response.feed;
+            for(var i = 0; i< page.feed.data.length; i++) {
+                page.setObject(page.feed.data[i].object_id, page.feed.data[i].status_type, i);
             }
         });
     };
-    $scope.getNextPage= function(){
-        Facebook.api($scope.feed.paging.next.substring(31), {access_token:$scope.token}, function(response) {
+    
+    page.getProfilePicture = function(id) {
+        Facebook.api(id, {access_token: page.token}, function(response) {
+            page.profilePicture = response.images[0].source;
+            console.log(page.profilePicture);
+
+        });
+    };
+    
+    page.getNextPage= function(){
+        Facebook.api(page.feed.paging.next.substring(31), {access_token:page.token}, function(response) {
             console.log(response);
-            $scope.feed = response;
-            for(var i = 0; i< $scope.feed.data.length; i++) {
-                $scope.setContent($scope.feed.data[i].object_id,$scope.feed.data[i].status_type, i);
+            page.feed = response;
+            for(var i = 0; i< page.feed.data.length; i++) {
+                page.setObject(page.feed.data[i].object_id, page.feed.data[i].status_type, i);
             }
             console.log(response);
         })
     };
     
-    $scope.getPreviousPage= function(){
-        Facebook.api($scope.feed.paging.previous.split(31), {access_token:$scope.token}, function(response) {
-            $scope.feed = response.feed;
+    page.getPreviousPage= function(){
+        Facebook.api(page.feed.paging.previous.split(31), {access_token:page.token}, function(response) {
+            page.feed = response.feed;
             console.log(response);
         })
     };
     
-    $scope.getFeed();
-
-        
-    // status_type from { mobile_status_update, created_note, added_photos, added_video, shared_story, created_group, created_event, wall_post, app_created_story, published_story, tagged_in_photo, approved_friend} //
+    page.setCover = function(coverId) {
+         Facebook.api(coverId, {access_token:page.token}, function(response) {
+            page.coverURL = response.images[0].source;
+             console.log(response.images[0].source);
+        });
+    };
     
-    $scope.setContent = function(object_id, status_type, index) {
+    page.setObject = function(object_id, status_type, index) {
         if(status_type=='added_photos'){
-            $scope.setImage(object_id, index);
+            page.setImage(object_id, index);
         }
       //  else if(status_type == 'added_video'){
       //  }
         else if(status_type == 'created_event'){
-            $scope.setEvent(object_id, index);
+            page.setEvent(object_id, index);
         }
         else if(status_type == 'shared_story'){
-            $scope.setStory(object_id, index);
+            page.setStory(object_id, index);
         }
     };
     
-    $scope.setStory  = function(object_id, index){
+    page.setStory  = function(object_id, index){
         if(object_id) {
-            Facebook.api(object_id, {access_token:$scope.token}, function(response){
-                $scope.feed.data[index].imageSource = response.images[0].source;
+            Facebook.api(object_id, {access_token:page.token}, function(response){
+                page.feed.data[index].imageSource = response.images[0].source;
             });
         }
     }
-    $scope.setEvent = function(object_id, index) {
+    page.setEvent = function(object_id, index) {
         if(object_id){
-            Facebook.api(object_id + '?fields=cover,description', {access_token:$scope.token}, function(response){
-                $scope.feed.data[index].message = response.description;
-                $scope.feed.data[index].startTime = response.startTime;
-                $scope.feed.data[index].imageSource = response.cover.source;
+            Facebook.api(object_id + '?fields=cover,description', {access_token:page.token}, function(response){
+                page.feed.data[index].message = response.description;
+                page.feed.data[index].startTime = response.startTime;
+                page.feed.data[index].imageSource = response.cover.source;
                 //console.log($scope.feed.data[index].imageSource);
             });
         }
         else{
-             $scope.feed.data[index].imageSource = null;
+             page.feed.data[index].imageSource = null;
         }
     }
     
-    $scope.setImage = function(object_id, index){
+    page.setImage = function(object_id, index){
        //console.log(post);
         if(object_id){
-            Facebook.api(object_id, {access_token:$scope.token}, function(response){
-                $scope.feed.data[index].imageSource = response.images[0].source;
-                console.log($scope.feed.data[index].imageSource);
+            Facebook.api(object_id, {access_token:page.token}, function(response){
+                page.feed.data[index].imageSource = response.images[0].source;
+               // console.log(page.feed.data[index].imageSource);
             });
         }
         else{
-             $scope.feed.data[index].imageSource = null;
+            // page.feed.data[index].imageSource = null;
         }
     }; 
+    
+    return page;
+    
+}])
+.controller('feedCtrl', ['$scope', 'Facebook', 'page', function($scope, Facebook, page){
+
 }])
 .controller('contactCtrl', ['$scope', function($scope){
 }])
 
-.controller('appCtrl', ['$scope', 'Facebook',function($scope, Facebook){
-    $scope.pageId = '1425194534381693';
-    $scope.facebookReady = false;
-    $scope.tabs = {};
-    $scope.tabs.gallery = false;
-    $scope.tabs.events = false;
-    $scope.token = '1427747097519995|85GJFSbBD6WbpVoF8EZavlLWKUE';
-    
+.controller('appCtrl', ['$scope','page', 'Facebook',function($scope,page, Facebook){
+
+    $scope.page = page;
     $scope.$watch(function() {  
       // This is for convenience, to notify if Facebook is loaded and ready to go.
       return Facebook.isReady();
     }, function(newVal) {
       // You might want to use this to disable/show/hide buttons and else
-        $scope.loadContent();
+        $scope.page.loadMainContent();
+        $scope.page.getFeed();
     });
-    
-    $scope.loadContent = function(){
-        Facebook.api($scope.pageId, {access_token:$scope.token}, function(response){
-            $scope.content = response;
-            $scope.facebookReady = true;
-            console.log('ready');
-        });
-    };
+
 }])
 .controller('navigationCtrl', ['$scope', function($scope){
 }])
-.controller('headerCtrl',['$scope','Facebook', function($scope, Facebook){
+.controller('headerCtrl',['$scope','Facebook','page', function($scope, Facebook, page){
     
-        
-    $scope.$watch(function() {  
-      // This is for convenience, to notify if Facebook is loaded and ready to go.
-      return $scope.facebookReady;
-    }, function(newVal) {
-      // You might want to use this to disable/show/hide buttons and else
-      if($scope.facebookReady) {$scope.loadContent();}
-    });
-      
-    $scope.setCover = function(coverId) {
-         Facebook.api(coverId, {access_token:$scope.token}, function(response) {
-            $scope.coverURL = response.images[0].source;
-             console.log(response.images[0].source);
-        });
-    };
-    
-    $scope.loadContent = function(){
-        $scope.setCover($scope.content.cover.cover_id);
-    };
+
 }])
 
-.controller('aboutCtrl', ['$scope', 'Facebook', function($scope, Facebook){ 
-    $scope.loadContent = function(){
-        Facebook.api($scope.pageId + '?fields=photos{picture,album},phone,about,emails,current_location,locations,description,company_overview,hours,price_range',{access_token: $scope.token}, function(response){
-            $scope.phone = response.phone;
-            $scope.about = response.about;
-            $scope.emails = response.emails;
-            $scope.currentLocation = response.current_location;
-            $scope.locations = response.locations;
-            $scope.description = response.description;
-            $scope.companyOverview = response.company_overview;
-            $scope.hours = response.hours;
-            $scope.priceRange = response.price_range;
-            console.log(response);
-            $scope.getProfilePicture(response.photos.data[0].id);
-        } );
-    }
-    $scope.getProfilePicture = function(id) {
-        Facebook.api(id, {access_token: $scope.token}, function(response) {
-            $scope.profilePicture = response.images[0].source;
-        });
-    };
-    $scope.loadContent();
+.controller('aboutCtrl', ['$scope', 'Facebook', 'page', function($scope, Facebook, page){ 
+
 }]);
 
 angular.module('app.filters', [])
